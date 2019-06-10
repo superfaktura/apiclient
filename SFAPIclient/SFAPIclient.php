d4ffbdc8f94df0a2a4a2af6d032a8bcadbb65a2a
Superfaktura <info@superfaktura.sk>
2019-06-10 08:56:31 +0200

    Update @version
    - add tag for packagist

--- SFAPIclient/SFAPIclient.php
+++ SFAPIclient/SFAPIclient.php
@@ -2,7 +2,7 @@
 /**
  * @category   SuperFaktura API
  * @author     SuperFaktura.sk s.r.o. <info@superfaktura.sk>
- * @version    1.19
+ * @version    1.22.1
  * @link https://github.com/superfaktura/docs
  * @lastUpdate 06.06.2019
  *
