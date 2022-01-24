0eb0786ec4024ead2003bcc00c6bd1e80fd834a9
SuperFaktura <info@superfaktura.sk>
2022-01-24 11:11:16 +0100

    Update sample.php

--- examples/sample.php
+++ examples/sample.php
@@ -1,10 +1,14 @@
 <?php
-	DEFINE('SFAPI_EMAIL', 'example@example.com');
-	DEFINE('SFAPI_KEY', 'apikey');
+	DEFINE('SFAPI_EMAIL', 'example@example.com'); 		 // LOGIN EMAIL TO SUPERFAKTURA
+	DEFINE('SFAPI_KEY', 'apikey'); 				  		 // SFAPI KEY
+	DEFINE('SFAPI_MODULE', 'API'); 				  		 // TITLE OF MODULE FE. 'WOOCOMMERCE MODULE'
+	DEFINE('SFAPI_APPTITLE', 'Example API application'); // TITLE OF YOUR APPLICATION FE. 'SUPERFAKTURA.SK'
+	DEFINE('COMPANY_ID', 1); // COMPANY_ID (optional)
 
 	require_once('../SFAPIclient/SFAPIclient.php');
 
-	$api = new SFAPIclient(SFAPI_EMAIL, SFAPI_KEY);
+	// Create and init SFAPIclient
+	$api = new SFAPIclient(SFAPI_EMAIL, SFAPI_KEY, SFAPI_APPTITLE, SFAPI_MODULE, COMPANY_ID);
 
 	//setup client data
 	$api->setClient(array(
